b5f3ca52b208d85c7552d9a831b5fb671a3857cc
unknown <sbd12b@gmail.com>
2019-06-08 21:08:08 -0400

    Add pagination IF Param -- tofix

--- app/routes/sean-daniels-api.js
+++ app/routes/sean-daniels-api.js
@@ -8,7 +8,17 @@ export default Ember.Route.extend({
         },
     },
     model(params) {
-        return this.get('ajax').request('https://jsonplaceholder.typicode.com/posts');
+        return this.get('ajax').request('https://jsonplaceholder.typicode.com/posts').then((response) => {
+            let pageNumber = 1;
+            for (let i = 1; i < response.length; i++ ) {
+                response[i].pageToShowOn = pageNumber;
+                if (i % 10 == 0 ) {
+                    pageNumber++;
+                }
+            }
+            console.log(response);
+            return response;
+        });
     }
 });
 
